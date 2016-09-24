2fc404e171e270a4cef84c8849a26c58b840c653
Theo Chatzimichos <tampakrap@gmail.com>
2016-09-24 17:42:08 +0200

    fix fetch cpan script to properly unpack the compressed data

--- bin/fetch_cpan.org.py
+++ bin/fetch_cpan.org.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python
 
+import gzip
 import json
-import urllib2
+import re
+import urllib
 import yaml
 
 # TODO: convert this to perl for the lulz
@@ -9,12 +11,26 @@ import yaml
 with open('etc/pkgmon.yml', 'r') as f:
     CONFIG = yaml.load(f)
 
-cpan_raw = urllib2.urlopen('http://www.cpan.org/modules/02packages.details.txt.gz')
+#urllib.urlretrieve('http://www.cpan.org/modules/02packages.details.txt.gz', '%s/cpan.txt.gz' % CONFIG['cache_dir'])
+with gzip.open('%s/cpan.txt.gz' % CONFIG['cache_dir'], 'rb') as f:
+    cpan_raw = f.read()
 
 packages = {}
-for line in cpan_raw:
+count = 0
+for line in cpan_raw.split('\n'):
+    count += 1
+    if count <= 9:
+        continue
     pkg = line.split('/')[-1].split('-')
-    packages['-'.join(pkg[0:-1])] = pkg[-1].replace('.tar.gz', '').strip()
+    pkg_name = '-'.join(pkg[0:-1])
+    pkg_version = pkg[-1].replace('.tar.gz', '').strip()
+    try:
+        packages[pkg_name].append(pkg_version)
+    except KeyError:
+        packages[pkg_name] = [pkg_version]
+del packages['']
+for package, versions in packages.iteritems():
+    packages[package] = max(versions)
 packages_json = json.dumps(packages, sort_keys=True, indent=4)
 with open('%s/cpan.org.json' % CONFIG['cache_dir'], 'w') as f:
     f.write(packages_json)
